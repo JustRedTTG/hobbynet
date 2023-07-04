3bca33d1660bdf8be70959b31ef881f07d09a1d4
red <red@redttg.com>
2023-07-05 01:50:25 +0300

    Add slug protection and 404

--- hobbynet/profiles/views.py
+++ hobbynet/profiles/views.py
@@ -1,9 +1,21 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import Http404
 from django.shortcuts import render, redirect
 
+UserModel: User = get_user_model()
 
 def profile_details(request, pk, slug):
-    return render(request, 'profiles/profile.html')
+    user = UserModel.objects.get(pk=pk)
+    if not user:
+        raise Http404("User profile doesn't exist")
+    if user.profile.slug != slug:
+        return redirect('profile_details', pk=pk, slug=user.profile.slug)
+    return render(request, 'profiles/profile.html', context={
+        'user': user,
+        'slug': slug
+    })
 
 
 class ProfileEdit:
