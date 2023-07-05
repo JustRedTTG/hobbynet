6b02390ca4723b1ca862fd5678ccc50149ce9f20
red <red@redttg.com>
2023-07-05 11:07:10 +0300

    basic base inheritance and css

--- hobbynet/common/views.py
+++ hobbynet/common/views.py
@@ -1,3 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+import django.views.generic as views
 
-# Create your views here.
+UserModel = get_user_model()
+
+class HomeView(views.ListView):
+    model = UserModel
+    template_name = 'common/home.html'
--- hobbynet/settings.py
+++ hobbynet/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'hobbynet.app_auth',
+    'hobbynet.common',
 ]
 
 MIDDLEWARE = [
@@ -85,6 +86,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
