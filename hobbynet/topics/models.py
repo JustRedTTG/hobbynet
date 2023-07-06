47326516b707eba52319cc3648c289d9a97a3a1d
red <red@redttg.com>
2023-07-06 13:45:30 +0300

    add topics model

--- hobbynet/topics/models.py
+++ hobbynet/topics/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+UserModel = get_user_model()
+
+class Topic(models.Model):
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+
+    privacy = models.CharField(max_length=10, choices=getattr(settings, 'PRIVACY_MODEL_CHOICES', None))
+
+    title = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'[{self.privacy}][{self.user}] {self.title}'
