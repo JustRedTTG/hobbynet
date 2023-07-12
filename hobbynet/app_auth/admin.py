1af2a1231326b27610217529a674e5176b241e87
red <red@redttg.com>
2023-07-12 17:31:29 +0300

    fix admin

--- hobbynet/app_auth/admin.py
+++ hobbynet/app_auth/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
 
 UserModel = get_user_model()
 
 # Register your models here.
 
-admin.site.register(UserModel)
+admin.site.register(UserModel, UserAdmin)
