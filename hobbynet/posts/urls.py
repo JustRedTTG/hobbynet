6f79451dea982ccdd9d1f0860c34f5f14c5afdc7
red <red@redttg.com>
2023-07-14 00:05:47 +0300

    fixing pathing and slug protection

--- hobbynet/posts/urls.py
+++ hobbynet/posts/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path, include
 from . import views
-from . import views
 
 urlpatterns = [
-    path('', include([
-        path('<int:user_id>_<slug:profile_slug>/<int:topic_id>/<int:pk>',
-             views.post_details, name='post_details'),
-        path('new',
-             views.PostCreationView.as_view(), name='post_create'),
-        # path('<int:user_id>_<slug:profile_slug>/<int:topic_id>/edit',
-        #      views.PostEditView.as_view(), name='post_edit')
-    ]))
+    path('<int:user_pk>/<slug:profile_slug>/<int:topic_pk>/<slug:topic_slug>/<int:pk>/<slug:slug>',
+         include([
+             path('', views.post_details, name='post_details'),
+             # path('edit', views.PostEditView.as_view() name='post_edit'),
+         ])),
+    path('new', views.PostCreationView.as_view(), name='post_create')
 ]
--- hobbynet/posts/views.py
+++ hobbynet/posts/views.py
@@ -2,17 +2,40 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
+from django.http import Http404
 from django.shortcuts import render, redirect
 from django.views.generic import FormView
 
 from hobbynet.posts.forms import PostForm
+from hobbynet.posts.models import Post
 from hobbynet.topics.models import Topic
 
 UserModel = get_user_model()
 
 
-def post_details(request, user_id, profile_slug, topic_id, pk):
-    user = UserModel.objects.get(id=user_id)
+def post_details(request, user_pk, profile_slug, topic_pk, topic_slug, pk, slug):
+    user = UserModel.objects.get(pk=user_pk)
+    topic: Topic = user.topic_set.get(pk=topic_pk)
+    post: Post = user.post_set.get(pk=pk)
+
+    if not user or (
+            request.user != user
+            and
+            not (request.user.is_superuser
+                 or
+                 (request.user.is_staff and request.user.has_perm('posts.view_post')))
+            and
+            post.visibility != 'public'
+    ):
+        raise Http404("Post doesn't exist")
+
+    if user.profile.slug != profile_slug or topic.slug != topic_slug or post.slug != slug:
+        return redirect(
+            'post_details',
+            user_pk=user.pk, profile_slug=user.profile.slug,
+            topic_pk=topic.pk, topic_slug=topic.slug,
+            pk=post.pk, slug=post.slug
+        )
 
     return render(request, 'posts/post.html', {
         'post': user.post_set.get(id=pk)
